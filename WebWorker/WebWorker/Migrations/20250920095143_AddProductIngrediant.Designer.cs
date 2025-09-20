418dc1806131742f109f0393f9a708c674d72eca
Mirada2701 <mirada-24@ukr.net>
2025-09-20 12:53:04 +0300

    Is Add ProductIngrediants

--- WebWorker/WebWorker/Migrations/20250920095143_AddProductIngrediant.Designer.cs
+++ WebWorker/WebWorker/Migrations/20250920095143_AddProductIngrediant.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using WebWorker.Data;
@@ -11,9 +12,11 @@ using WebWorker.Data;
 namespace WebWorker.Migrations
 {
     [DbContext(typeof(AppWorkerDbContext))]
-    partial class AppWorkerDbContextModelSnapshot : ModelSnapshot
+    [Migration("20250920095143_AddProductIngrediant")]
+    partial class AddProductIngrediant
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
